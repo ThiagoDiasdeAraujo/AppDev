60a776d2fd0d69374487906dde4b23edaa2e8b60
thiago dias de araujo <thiago.diasdearaujo@student.odisee.be>
2023-11-07 23:55:17 +0100

    deadline

--- 00-CommeChezSwa/WebApp/Data/Reservatie.cs
+++ 00-CommeChezSwa/WebApp/Data/Reservatie.cs
@@ -1,17 +1,30 @@
 ﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Data
 {
     public class Reservatie
     {
         public int ReservatieID { get; set; }
+
         public string Voornaam { get; set; }
+
+        [Display(Name = "Aantal Personen")]
         public string Familienaam { get; set; }
+
         public string? Email { get; set; }
+
         //public string? EmailBevestigen { get; set; }
+
+        [Display(Name = "Datum Reservatie")]
+
         public DateTime? DatumReservatie { get; set; }
+
+        [Display(Name = "Aantal Personen")]
         public int? AantalPersonen { get; set; }
+
         //public bool NotARobot { get; set; }
+
         public Tijdslot Tijdslot { get; set; }
     }
 }
